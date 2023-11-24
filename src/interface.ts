6eba455501fca97f13c02d5de30e097ef1761104
Jan Schill <janschill@proton.me>
2023-11-24 18:50:09 +0100

    Interfaces for Endpoints

--- src/interface.ts
+++ src/interface.ts
@@ -1,25 +1,38 @@
-// TODO: Would be cool if these interfaces could be matched against what the FedCM IdP is returning.
-
 interface IdentityProviderWellKnown {
   provider_urls: string[];
 }
 
 interface IdentityProviderIcon {
   url: string;
-  size?: number; // Optional property
+  size?: number;
 }
 
 interface IdentityProviderBranding {
-  background_color?: string; // Optional property
-  color?: string; // Optional property
+  background_color?: string;
+  color?: string;
   icons: IdentityProviderIcon[];
-  name?: string; // Optional property
+  name?: string;
 }
 
 interface IdentityProviderAPIConfig {
   accounts_endpoint: string;
   client_metadata_endpoint: string;
   id_assertion_endpoint: string;
-  login_url: string; // Assuming login_url is a required property (not mentioned in the provided dictionary)
+  login_url: string;
   branding: IdentityProviderBranding;
 }
+
+interface IdentityProviderAccount {
+  id: string;
+  name: string;
+  email: string;
+  given_name: string;
+  picture: string;
+  approved_clients: string[];
+  login_hints: string[];
+}
+
+interface IdentityProviderAccountList {
+  accounts: IdentityProviderAccount[];
+}
+
